16cc5c3d2e19c75b03942ddc124ed83de3e91632
Yeonsu Jung <yeonsu@Yeonsus-MacBook-Pro.local>
2021-05-02 22:18:30 -0400

    added falkner skan scheme

--- analysis_protocol.py
+++ analysis_protocol.py
@@ -11,26 +11,27 @@ from importlib import reload
 # reload(matplotlib)
 # matplotlib.get_backend()
 # %%
-import matplotlib
-matplotlib.use('agg')
-matplotlib.get_backend()
+# import matplotlib
+# matplotlib.use('agg')
+# matplotlib.get_backend()
 # %%
 plt.plot([1,1],[2,2])
 
 # %%
-out_path = '/Users/yeonsu/Dropbox (Harvard University)/Riblet/data/piv-results/'
+out_path = Path('/Users/yeonsu/Dropbox (Harvard University)/Riblet/data/piv-results/')
+out_path.is_dir()
 
 # %%
-matches = Path(out_path).glob("**/u.txt")
+matches = out_path.glob("**/u.txt")
 matches_list = sorted(matches)
 
 # %%
 for x in matches_list:
-    if '2021-04-07' in str(x):
+    if '2021-04-06' in str(x):
         print(x)
 
 # %%
-matches_partial_list = [x for x in matches_list if '2021-04-07' in str(x)]
+matches_partial_list = [x for x in matches_list if '2021-04-06' in str(x)]
 len(matches_partial_list)
 
 # %%
@@ -80,7 +81,7 @@ for x in matches_partial_list:
         plt.plot(-v_data[i,k,xtmp<w1],x_data[0,xtmp<w1],'.')
         
     plt.plot(-v_data[:,k,xtmp<w1].mean(axis=0),x_data[0,xtmp<w1],'k-',linewidth=2)
-    plt.title(str(x.parent).replace(out_path,''))
+    # plt.title(str(x.parent).replace(out_path,''))
     left, right = plt.xlim()
 
     plt.plot([0, right],[w1,w1],'k--')    
@@ -98,13 +99,31 @@ for x in matches_partial_list:
     plt.close()
 
 # %%
-for x in matches_partial_list:
+
+
+# %%
+matches = out_path.glob("**/u.txt")
+matches_list = sorted(matches)
+
+# %%
+for x in matches_list:
+    if '2021-04-06' in str(x) and 'Flat_10 (black)_motor10_particle4_hori1280_laser1-4_nd0p7/0,0,0,0_32,26,50/series_003_95' in str(x):
+        print(x)
+
+# %%
+matches_partial_list = [x for x in matches_list if '2021-04-06' in str(x)]
+len(matches_partial_list)
+
+# %%
+# matches_partial_list = [Path('/Users/yeonsu/Dropbox (Harvard University)/Riblet/data/piv-results/2021-04-06/Flat_10 (black)_motor10_particle4_hori1280_laser1-4_nd0p7/0,0,0,0_32,26,50/series_003_95/u.txt')]
+
+for x in matches_list:
     setting_path = os.path.join(x.parent,'piv_setting.yaml')
     with open(setting_path) as f:
         piv_setting = yaml.safe_load(f)
     
     xpath = os.path.join(x.parent,'x.txt')
-    ypath = os.path.join(x.parent,'x.txt')
+    ypath = os.path.join(x.parent,'y.txt')
     upath = os.path.join(x.parent,'u.txt')
     vpath = os.path.join(x.parent,'v.txt')
 
@@ -113,50 +132,46 @@ for x in matches_partial_list:
     u_data = load_nd_array(upath)
     v_data = load_nd_array(vpath)
 
-    w1,w2 = np.loadtxt(x.parents[2].joinpath('wall_a_position.txt'))
+    u_mean = np.mean(u_data,axis=0)
+    v_mean = np.mean(v_data,axis=0)
 
-    pix_den = piv_setting['pixel_density']
+    u_sd = np.std(u_data,axis = 0, ddof=1)
+    v_sd = np.std(v_data,axis = 0, ddof=1)
 
-    w1 = w1/pix_den
-    w2 = w2/pix_den
-   
-    xtmp = x_data[0,:]
+    u_se = u_sd / np.sqrt(u_sd.shape[0])
+    v_se = v_sd / np.sqrt(v_sd.shape[0])
 
-    k = 15
-    fig = plt.figure(figsize=(15,5))    
-    plt.subplot(1,2,1)
-    for i in range(u_data.shape[0]):
-        plt.plot(-v_data[i,k,xtmp<w1],x_data[0,xtmp<w1],'.')
-        
-    plt.plot(-v_data[:,k,xtmp<w1].mean(axis=0),x_data[0,xtmp<w1],'k-',linewidth=2)
-    plt.title(str(x.parent).replace(out_path,''))
-    left, right = plt.xlim()
-
-    plt.plot([0, right],[w1,w1],'k--')    
-    plt.xlim([0, right])
-
-    plt.subplot(1,2,2)
-    for i in range(u_data.shape[0]):
-        plt.plot(-v_data[i,k,xtmp>w2],x_data[0,xtmp>w2],'.')
-
-    plt.plot(-v_data[:,k,xtmp>w2].mean(axis=0),x_data[0,xtmp>w2],'k-',linewidth=2)
-    plt.plot([0,right],[w2,w2],'k--')
-    plt.xlim([0,right])       
+    np.savetxt(x.parent.joinpath('u_mean.txt'),u_mean)
+    np.savetxt(x.parent.joinpath('v_mean.txt'),v_mean)
+    np.savetxt(x.parent.joinpath('u_sd.txt'),u_sd)
+    np.savetxt(x.parent.joinpath('v_sd.txt'),v_sd)
+    np.savetxt(x.parent.joinpath('u_se.txt'),u_se)
+    np.savetxt(x.parent.joinpath('v_se.txt'),v_se)
     
-    plt.savefig(x.parent.joinpath('raw+mean.png'))
-    plt.close()
 
 # %%
-out_path = Path('/Users/yeonsu/Dropbox (Harvard University)/Riblet/data/piv-results/')
-# %%
-matches = out_path.glob('**/raw+mean.png')
-matches = sorted(matches)
+
 
 # %%
-for x in matches:
+plt.figure(figsize=(20,10))
+plt.subplot(2,1,1)
+plt.errorbar(x_data[0,:],-v_mean[0,:],tst[0,:],capsize=5,fmt='o-')
+plt.subplot(2,1,2)
+plt.errorbar(x_data[0,:],-v_mean[0,:],-v_sd[0,:]/np.sqrt(95),capsize=5,fmt='o-')
+
+# %%
+rel_path = '2021-04-06/Flat_10 (black)_motor25_particle4_hori1408_laser1-4_nd0p7/0,0,0,0_32,26,50/series_003_95'
+pth = out_path.joinpath(rel_path)
+for x in pth.iterdir():
     print(x)
 
+# %%
+matches = out_path.glob('**/u.txt')
+matches = sorted(matches)
 
+for x in matches:
+    if '2021-04-06' in str(x):
+        print(x)
 
 # %%
 x.parents[2]
