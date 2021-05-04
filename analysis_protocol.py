2fbbcf59e5233d47bc9ccd38d40f61c47a6d9495
Yeonsu Jung <yeonsu@Yeonsus-MacBook-Pro.local>
2021-05-03 23:11:12 -0400

    Update analysis_protocol.py

--- analysis_protocol.py
+++ analysis_protocol.py
@@ -3,37 +3,35 @@ import numpy as np
 import os
 import re
 import yaml
+import matplotlib.image as mpimg
+
 from matplotlib import pyplot as plt
 from pathlib import Path
 from scipy.optimize import curve_fit
+from scipy.signal import find_peaks
+from scipy.signal import argrelmax
+
+# from medpy.filter.smoothing import anisotropic_diffusion
+
 from importlib import reload
-# %%
-# import matplotlib
-# reload(matplotlib)
-# matplotlib.get_backend()
-# %%
-# import matplotlib
-# matplotlib.use('agg')
-# matplotlib.get_backend()
+
+from falkner_skan import falkner_skan
+
 
 # %%
+base_path = Path('/Users/yeonsu/Dropbox (Harvard University)/Riblet/data/piv-data/')
 out_path = Path('/Users/yeonsu/Dropbox (Harvard University)/Riblet/data/piv-results/')
-out_path.is_dir()
+base_path.is_dir(),out_path.is_dir()
 
 # %%
-matches = out_path.glob("**/u.txt")
-matches_list = sorted(matches)
+def base_to_out(path_in):
+    assert isinstance(path_in,Path)        
+    return out_path.joinpath(path_in.relative_to(base_path))
 
-# %%
-for pth in matches_list:
-    if '2021-04-06' in str(pth):
-        print(pth)
+def out_to_base(path_in):
+    assert isinstance(path_in,Path)
+    return base_path.joinpath(path_in.relative_to(out_path))
 
-# %%
-matches_partial_list = [x for x in matches_list if '2021-04-06' in str(x)]
-len(matches_partial_list)
-
-# %%
 def load_nd_array(path):
     with open(path, 'r') as file:
         temp = file.readline()            
@@ -47,82 +45,174 @@ def load_nd_array(path):
         ar = ar.reshape(field_shape)        
     return ar
 
+def get_wall_pos2(img, check_img = False, check_plots = False): # img or img path
+    num_col = img.shape[1]
+    row_averaged = np.zeros(num_col)
+    for i in range(num_col):
+        row_averaged[i] = np.sum(img[:,i])    
+
+    peaks, _ = find_peaks(row_averaged,distance=100)
+    peaks = sorted(peaks,key = lambda x: -row_averaged[x])
+    
+    idx1 = min(peaks[0],peaks[1])
+    idx2 = max(peaks[0],peaks[1])
+
+    w1_new = np.zeros(img.shape,dtype=bool)
+    w2_new = np.zeros(img.shape,dtype=bool)
+
+    w1_new[:,idx1] = 1    
+    w2_new[:,idx2] = 1
+
+    if check_img:
+        row_dim = img.shape[0]
+        plt.imshow(img)
+        plt.plot([idx1,idx1],[0,row_dim],'r-',linewidth=0.5)
+        plt.plot([idx2,idx2],[0,row_dim],'r-',linewidth=0.5)
+        plt.show()
+
+    if check_plots:        
+        plt.plot(row_averaged,'o-')        
+        # plt.plot(row_averaged[peaks],'o-')
+        plt.show()
+
+    return idx1, idx2, w1_new, w2_new
+
+def fs_query(eta_q,delta_eta,m,U0,p):
+    eta,f0,f1,f2 = falkner_skan(m)    
+    return np.interp(eta_q,p*(eta-delta_eta),f1)*U0
+
 # %%
-# matplotlib.use('Agg')
-for pth in matches_partial_list:
-    setting_path = os.path.join(pth.parent,'piv_setting.yaml')
-    with open(setting_path) as f:
-        piv_setting = yaml.safe_load(f)
+
+def get_wall_pos3(img, check_img = False, check_plots = False):    
+    def foo(arr):
+        idx = np.argsort(2*arr[1:-1] - arr[:-2] - arr[2:])        
+        return idx[-1]+1, idx[-2]+1
+
+    num_col = img.shape[1]
+    row_averaged = np.zeros(num_col)
+    for i in range(num_col):
+        row_averaged[i] = np.sum(img[:,i])  
+
+    center = np.argmin(row_averaged)
+
+    # peaks = argrelmax(row_averaged,order = 10)
+
+    max_loc = argrelmax(row_averaged,order = 10)        
+
+    # plt.plot(row_averaged,'o-')
+    # plt.plot(*max_loc,row_averaged[max_loc[0]],'o-')        
+    # plt.plot(row_averaged)
+    # plt.show()
     
-    xpath = os.path.join(pth.parent,'x.txt')
-    ypath = os.path.join(pth.parent,'x.txt')
-    upath = os.path.join(pth.parent,'u.txt')
-    vpath = os.path.join(pth.parent,'v.txt')
+    a,b = foo(row_averaged[max_loc[0]])
 
-    x_data = np.loadtxt(xpath)
-    y_data = np.loadtxt(ypath)
-    u_data = load_nd_array(upath)
-    v_data = load_nd_array(vpath)
+    # plt.plot(max_loc[0][a], row_averaged[max_loc[0][a]],'o')
+    # plt.plot(max_loc[0][b], row_averaged[max_loc[0][b]],'o')
+    # plt.show()
 
-    w1,w2 = np.loadtxt(pth.parents[2].joinpath('wall_a_position.txt'))
+    peaks = [max_loc[0][a] , max_loc[0][b]]
 
-    pix_den = piv_setting['pixel_density']
-
-    w1 = w1/pix_den
-    w2 = w2/pix_den
-   
-    xtmp = x_data[0,:]
-
-    k = 15
-    fig = plt.figure(figsize=(15,5))    
-    plt.subplot(1,2,1)
-    for i in range(u_data.shape[0]):
-        plt.plot(-v_data[i,k,xtmp<w1],x_data[0,xtmp<w1],'.')
-        
-    plt.plot(-v_data[:,k,xtmp<w1].mean(axis=0),x_data[0,xtmp<w1],'k-',linewidth=2)
-    # plt.title(str(x.parent).replace(out_path,''))
-    left, right = plt.xlim()
-
-    plt.plot([0, right],[w1,w1],'k--')    
-    plt.xlim([0, right])
-
-    plt.subplot(1,2,2)
-    for i in range(u_data.shape[0]):
-        plt.plot(-v_data[i,k,xtmp>w2],x_data[0,xtmp>w2],'.')
-
-    plt.plot(-v_data[:,k,xtmp>w2].mean(axis=0),x_data[0,xtmp>w2],'k-',linewidth=2)
-    plt.plot([0,right],[w2,w2],'k--')
-    plt.xlim([0,right])       
+    # print(peaks)
     
-    plt.savefig(pth.parent.joinpath('raw+mean.png'))
-    plt.close()
+    idx1 = min(peaks[0],peaks[1])
+    idx2 = max(peaks[0],peaks[1])
 
+    w1_new = np.zeros(img.shape,dtype=bool)
+    w2_new = np.zeros(img.shape,dtype=bool)
 
+    w1_new[:,idx1] = 1    
+    w2_new[:,idx2] = 1
+
+    if check_img:
+        row_dim = img.shape[0]
+        plt.imshow(img)
+        plt.plot([idx1,idx1],[0,row_dim],'r-',linewidth=0.5)
+        plt.plot([idx2,idx2],[0,row_dim],'r-',linewidth=0.5)
+        plt.show()
+
+    if check_plots:        
+        plt.subplot(2,1,1)
+        plt.plot(row_averaged,'o-')        
+        bottom, top = plt.ylim()
+        plt.plot([center,center],[bottom,top])        
+        plt.plot(peaks,row_averaged[peaks],'o')        
+        plt.show()
+
+    return idx1, idx2, w1_new, w2_new
 # %%
 matches = out_path.glob("**/u.txt")
 matches_list = sorted(matches)
 
-# %%
-for pth in matches_list:
-    if '2021-04-06' in str(pth) and 'Flat_10 (black)_motor10_particle4_hori1280_laser1-4_nd0p7/0,0,0,0_32,26,50/series_003_95' in str(pth):
-        print(pth)
+# %% TOTAL LIST
+containing_dir_list = []
+for pth in matches_list:   
+    info_crop = re.findall('(\d+),(\d+),(\d+),(\d+)',pth.parents[1].name)
+
+    assert len(info_crop) == 1
+    pos = (int(info_crop[0][0]),int(info_crop[0][1]),int(info_crop[0][2]),int(info_crop[0][3]))
+
+    if pos == (0,0,0,0):
+        info_img_pairs = re.findall('(\d+)_(\d+)',pth.parents[0].name)
+        assert len(info_img_pairs) == 1
+
+        start_index = int(info_img_pairs[0][0])
+        num_img_pairs = int(info_img_pairs[0][1])
+
+        if num_img_pairs > 60:
+            containing_dir_list.append(pth.parent)
+
+containing_dir_list
+
+# %% SHORTLISTING
+containing_dir_shortlist = []
+
+keyword1 = '2021-04-06'
+keyword2 = ''
+exclude1 = '1600'
+exclude2 = 'filled'
+for pth in containing_dir_list:
+    if keyword1 in str(pth) and keyword2 in str(pth) and not exclude1 in str(pth) and not exclude2 in str(pth):
+        containing_dir_shortlist.append(pth)
+
+len(containing_dir_shortlist)
 
 # %%
-matches_partial_list = [x for x in matches_list if '2021-04-06' in str(x)]
-len(matches_partial_list)
+
+img_dir_list = []
+for pth in containing_dir_shortlist:
+    img_dir_list.append(out_to_base(pth.parents[1]))
+
+img_dir_list
 
 # %%
-# matches_partial_list = [Path('/Users/yeonsu/Dropbox (Harvard University)/Riblet/data/piv-results/2021-04-06/Flat_10 (black)_motor10_particle4_hori1280_laser1-4_nd0p7/0,0,0,0_32,26,50/series_003_95/u.txt')]
+ii = 0
+for img_dir in img_dir_list:
+    
+    img_a = mpimg.imread(img_dir.joinpath('frame_000010.tiff'))
+    img_b = mpimg.imread(img_dir.joinpath('frame_000011.tiff'))
 
-for pth in matches_list:
-    setting_path = os.path.join(pth.parent,'piv_setting.yaml')
+    wa1_pos, wa2_pos, wa1, wa2 = get_wall_pos3(img_a, check_img=False, check_plots = False)
+    wb1_pos, wb2_pos, wb1, wb2 = get_wall_pos3(img_b, check_img=False, check_plots = False)
+
+    wpos_a_path = base_to_out(img_dir).joinpath('wall_a_position.txt')
+    wpos_b_path = base_to_out(img_dir).joinpath('wall_b_position.txt')
+
+    np.savetxt(wpos_a_path,[wa1_pos,wa2_pos],fmt='%d')
+    np.savetxt(wpos_b_path,[wb1_pos,wb2_pos],fmt='%d')
+
+    # if abs(wa1_pos - wb1_pos) > 3 or abs(wa2_pos - wb2_pos) > 3:
+    #     print(img_dir.name)    
+
+# %%
+for pth in containing_dir_shortlist:
+    setting_path = os.path.join(pth,'piv_setting.yaml')
     with open(setting_path) as f:
         piv_setting = yaml.safe_load(f)
     
-    xpath = os.path.join(pth.parent,'x.txt')
-    ypath = os.path.join(pth.parent,'y.txt')
-    upath = os.path.join(pth.parent,'u.txt')
-    vpath = os.path.join(pth.parent,'v.txt')
+    xpath = os.path.join(pth,'x.txt')
+    ypath = os.path.join(pth,'y.txt')
+    upath = os.path.join(pth,'u.txt')
+    vpath = os.path.join(pth,'v.txt')
 
     x_data = np.loadtxt(xpath)
     y_data = np.loadtxt(ypath)
@@ -138,36 +228,21 @@ for pth in matches_list:
     u_se = u_sd / np.sqrt(u_sd.shape[0])
     v_se = v_sd / np.sqrt(v_sd.shape[0])
 
-    np.savetxt(pth.parent.joinpath('u_mean.txt'),u_mean)
-    np.savetxt(pth.parent.joinpath('v_mean.txt'),v_mean)
-    np.savetxt(pth.parent.joinpath('u_sd.txt'),u_sd)
-    np.savetxt(pth.parent.joinpath('v_sd.txt'),v_sd)
-    np.savetxt(pth.parent.joinpath('u_se.txt'),u_se)
-    np.savetxt(pth.parent.joinpath('v_se.txt'),v_se)
-    
-# %%
-import falkner_skan
-reload(falkner_skan)
-from falkner_skan import falkner_skan
+    np.savetxt(pth.joinpath('u_mean.txt'),u_mean)
+    np.savetxt(pth.joinpath('v_mean.txt'),v_mean)
+    np.savetxt(pth.joinpath('u_sd.txt'),u_sd)
+    np.savetxt(pth.joinpath('v_sd.txt'),v_sd)
+    np.savetxt(pth.joinpath('u_se.txt'),u_se)
+    np.savetxt(pth.joinpath('v_se.txt'),v_se)
 
-# %%
-falkner_skan(-0.02)
-# %%
-def fs_query(eta_q,delta_eta,m,U0,p):
-    eta,f0,f1,f2 = falkner_skan(m)    
-    return np.interp(eta_q,p*(eta-delta_eta),f1)*U0
 # %%
 def fs_wall_shear(m):
     eta,f0,f1,f2 = falkner_skan(m)
     return f2[0]
 
-m = 0
-f20 = fs_wall_shear(m)
-f20*1e-3*((m+1)/2)**0.5*(346**3/1e-6/50)**0.5*1e-3
-# %%
 def falkner_scan_fitting(xx,yy,ee,img_path):
     _p0 = (0,0,600,10)
-    _bounds = ((-1,-0.08,200,0.1),(1,3,1200,100))
+    _bounds = ((-10,-0.08,200,0.1),(10,3,1200,100))
     
     popt,pcov = curve_fit(fs_query,xx,yy,p0=_p0,bounds=_bounds)
     perr = np.sqrt(np.diag(pcov))
@@ -191,12 +266,13 @@ def falkner_scan_fitting(xx,yy,ee,img_path):
     xtmp2 = np.linspace(-delta_y,np.max(xtmp),100)
 
     plt.errorbar(xx,yy,ee,fmt='o',capsize=4)
-    plt.plot(xtmp2,fs_query(xtmp2,*popt))
-
-    plt.ylim((0,popt[2]*1.2))
 
     left, right = plt.xlim()
     bottom, top = plt.ylim()
+
+    plt.plot(xtmp2,fs_query(xtmp2,*popt))
+
+    plt.ylim((0,popt[2]*1.2))    
 
     plt.title('%d,%d,%d,%d_%d,%d,%d,%d-%d,%d,%d,%d'%(*_p0,*_bounds[0],*_bounds[1]))
     plt.text(0.5*(right-left),0.5*top,'m = %.2f $\pm$ %.3f'%(popt[0],perr[0]))
@@ -210,47 +286,23 @@ def falkner_scan_fitting(xx,yy,ee,img_path):
 
     plt.savefig(img_path)
     plt.close()
-# %%
-lst = [Path('/Users/yeonsu/Dropbox (Harvard University)/Riblet/data/piv-results/2021-04-06/Flat_10 (black)_motor25_particle4_hori1408_laser1-4_nd0p7/0,0,0,0_32,26,50/series_003_95/u.txt')]
-
-matches_partial_list = []
-
-for pth in matches_list:
-    if '2021-04-06' in str(pth) and '003_95' in str(pth):
-
-        numbers = re.findall('(\d+),(\d+),(\d+),(\d+)',str(pth))
-
-        assert len(numbers) == 1
-        pos = (int(numbers[0][0]),int(numbers[0][1]),int(numbers[0][2]),int(numbers[0][3]))
-
-        print(pth)
-
-        if pos == (0,0,0,0):
-            matches_partial_list.append(pth)
-            print(pth)      
-        
-# %%
-
 
 # %%
-print(len(matches_partial_list))
+for pth in containing_dir_shortlist:
+    xpath = os.path.join(pth,'x.txt')
+    ypath = os.path.join(pth,'y.txt')
+    upath = os.path.join(pth,'u.txt')
+    vpath = os.path.join(pth,'v.txt')
 
-for pth in matches_partial_list:
-    print(pth.parent.parent.parent.name)
-
-# %%
-for pth in matches_partial_list[9:10]:
-    xpath = os.path.join(pth.parent,'x.txt')
-    ypath = os.path.join(pth.parent,'y.txt')
-    upath = os.path.join(pth.parent,'u.txt')
-    vpath = os.path.join(pth.parent,'v.txt')
-
-    w1,w2 = np.loadtxt(pth.parents[2].joinpath('wall_a_position.txt'))
+    w1,w2 = np.loadtxt(pth.parents[1].joinpath('wall_a_position.txt'))
 
     x_data = np.loadtxt(xpath)
     y_data = np.loadtxt(ypath)
     u_data = load_nd_array(upath)
     v_data = load_nd_array(vpath)
+    
+    with open(pth.joinpath('piv_setting.yaml')) as f:
+        piv_param = yaml.safe_load(f)
 
     u_mean = np.mean(u_data,axis=0)
     v_mean = np.mean(v_data,axis=0)
@@ -261,158 +313,60 @@ for pth in matches_partial_list[9:10]:
     u_se = u_sd / np.sqrt(u_sd.shape[0])
     v_se = v_sd / np.sqrt(v_sd.shape[0])
 
-    w1_index = int((w1-26)//(32-26))
-    w2_index = int((w2-26)//(32-26))
+    winsize = piv_param['winsize']
+    overlap = piv_param['overlap']
+
+    w1_index = int((w1-overlap)//(winsize-overlap))
+    w2_index = int((w2-overlap)//(winsize-overlap))
 
     delta = 0
-    starting_index = w1_index - delta
-
-    x1 = -x_data[0,:starting_index]+x_data[0,starting_index]
-    y1 = -v_mean[0,:starting_index]
-    e1 = v_se[0,:starting_index]
-
-    falkner_scan_fitting(x1,y1,e1,pth.parent.joinpath('fitting_back_%d.png'%starting_index))
+    starting_index1 = w1_index - delta
 
     delta = 4
-    starting_index = w2_index + delta
+    starting_index2 = w2_index + delta
 
-    x2 = x_data[0,starting_index:]-x_data[0,starting_index]
-    y2 = -v_mean[0,starting_index:]
-    e2 = v_se[0,starting_index:]
+    si_pth = pth.joinpath(f'starting_index_{starting_index1}_{starting_index2}')
 
-    falkner_scan_fitting(x2,y2,e2,pth.parent.joinpath('fitting_front_%d.png'%starting_index))
+    if not si_pth.is_dir():
+        os.makedirs(si_pth)
+
+    for k in range(x_data.shape[0]):
+        x1 = -x_data[k,:starting_index1]+x_data[k,starting_index1]
+        y1 = -v_mean[k,:starting_index1]
+        e1 = v_se[k,:starting_index1]
+
+        falkner_scan_fitting(x1,y1,e1,si_pth.joinpath('fitting_back_%03d.png'%k))    
+
+        x2 = x_data[k,starting_index2:]-x_data[k,starting_index2]
+        y2 = -v_mean[k,starting_index2:]
+        e2 = v_se[k,starting_index2:]
+
+        falkner_scan_fitting(x2,y2,e2,si_pth.joinpath('fitting_front_%03d.png'%k))
+# %%
+out_to_base(pth)
 # %%
 pth
-
-# w2_index
 # %%
-y_data.shape
-
-
-
+starting_index1 = 1
+'starting_index_{si}'.format(si=starting_index1)
 # %%
-x2 = x_data[0,starting_index:]-x_data[0,starting_index]
-y2 = -v_mean[0,starting_index:]
-e2 = v_se[0,starting_index:]
-
-plt.plot(x2,y2,'o')
-
-x2.shape
-
+f'starting_index_{starting_index1}'
 
 # %%
-falkner_scan_fitting(x1,y1,e1,pth.parent.joinpath('fitting_back.png'))
-# %%
-pth = Path('/Users/yeonsu/Dropbox (Harvard University)/Riblet/data/piv-results/2021-04-06/Flat_10 (black)_motor25_particle4_hori1408_laser1-4_nd0p7/0,0,0,0_32,26,50/series_003_95/u.txt')
-
-xpath = os.path.join(pth.parent,'x.txt')
-ypath = os.path.join(pth.parent,'y.txt')
-upath = os.path.join(pth.parent,'u.txt')
-vpath = os.path.join(pth.parent,'v.txt')
-
-w1,w2 = np.loadtxt(pth.parents[2].joinpath('wall_a_position.txt'))
-
-x_data = np.loadtxt(xpath)
-y_data = np.loadtxt(ypath)
-u_data = load_nd_array(upath)
-v_data = load_nd_array(vpath)
-
-u_mean = np.mean(u_data,axis=0)
-v_mean = np.mean(v_data,axis=0)
-
-u_sd = np.std(u_data,axis = 0, ddof=1)
-v_sd = np.std(v_data,axis = 0, ddof=1)
-
-u_se = u_sd / np.sqrt(u_sd.shape[0])
-v_se = v_sd / np.sqrt(v_sd.shape[0])
-
-w1_index = int((w1-26)//(32-26))
-w2_index = int((w2-26)//(32-26))
-
-w1_index,w2_index
-# %%
-starting_index = w1_index
-
-xtmp = -x_data[0,:starting_index]+x_data[0,starting_index]
-ytmp = -v_mean[0,:starting_index]
-etmp = v_se[0,:starting_index]
-
-# plt.errorbar(ytmp,xtmp,xerr=etmp,fmt='o',capsize=3)
-# %%
-
-
-
+'Hey %(name)s, there is a 0x%(errno)x error!' % {
+...     "name": 'ddd', "errno": 2 }
 
 # %%
-
-popt,pcov = curve_fit(fs_query,xtmp,ytmp,p0=(0,0,800,10),bounds=((-1,-3,600,0.1),(1,3,950,100)))
-perr = np.sqrt(np.diag(pcov))
-rel_err = perr/popt
-
-nu = 1e-6
-L = 100
-U0 = popt[2]
-m = popt[1]
-pth = (2*nu*L/U0/(m+1)*L**(m-1)/popt[3]**2)*1e6
-x_err = pth*(rel_err[0] + rel_err[2] + 2*rel_err[3])
-delta_y = popt[0]*popt[3]
-delta_y_err = perr[0]*popt[3]
-
+ 'Hey {name}, there is a 0x{errno:x} error!'.format(
+...     name=name, errno=errno)
 # %%
-xtmp2 = np.linspace(-delta_y,np.max(xtmp),100)
+a=1
+b=2
 
-plt.errorbar(xtmp,ytmp,etmp,fmt='o',capsize=4)
-plt.plot(xtmp2,fs_query(xtmp2,*popt))
-
-plt.ylim((0,popt[2]*1.2))
-
-left, right = plt.xlim()
-bottom, top = plt.ylim()
-
-plt.text(0.5*right,0.5*top,'m = %.2f $\pm$ %.2f'%(popt[0],perr[0]))
-plt.text(0.5*right,0.5*top-100,'U0 = %.2f $\pm$ %.2f mm/s'%(popt[2],perr[2]))
-plt.text(0.5*right,0.5*top-200,'x = %.2f $\pm$ %.2f mm'%(pth,x_err))
-plt.text(0.5*right,0.5*top-300,'delta y = %.2f $\pm$ %.2f mm'%(delta_y,delta_y_err))
-
-plt.xlabel('y (mm)')
-plt.ylabel('u (mm)')
-
+f'Five plus ten is {a + b} and not {2 * (a + b)}.'
 # %%
-
-
-# %%
-
-
-# %%
-# plt.plot(v_mean[0,w2_index:],'o-')
-
-
-
-# %%
-plt.figure(figsize=(20,10))
-plt.subplot(2,1,1)
-plt.errorbar(x_data[0,:],-v_mean[0,:],tst[0,:],capsize=5,fmt='o-')
-plt.subplot(2,1,2)
-plt.errorbar(x_data[0,:],-v_mean[0,:],-v_sd[0,:]/np.sqrt(95),capsize=5,fmt='o-')
-
-# %%
-rel_path = '2021-04-06/Flat_10 (black)_motor25_particle4_hori1408_laser1-4_nd0p7/0,0,0,0_32,26,50/series_003_95'
-pth = out_path.joinpath(rel_path)
-for pth in pth.iterdir():
-    print(pth)
-
-# %%
-matches = out_path.glob('**/u.txt')
-matches = sorted(matches)
-
-for pth in matches:
-    if '2021-04-06' in str(pth):
-        print(pth)
-
-# %%
-pth.parents[2]
-
-# %%
-u_data.shape
-
-# %%
+print('''\
+    A
+    B
+    C
+    ''')
